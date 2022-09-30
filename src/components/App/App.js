bbc5d5f9645ba16b88990bf2ed0e697db1e730de
Aleksi Karelius <aleksi.karelius@tuni.fi>
2022-09-30 19:10:13 +0300

    Betteer routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,32 @@
 import './App.css';
 import React, {Component, useState} from 'react';
 import {
-    BrowserRouter as Router,
     Routes,
     Route,
-    Navigate,
-    Link,
-    Redirect, BrowserRouter
+    BrowserRouter
 } from 'react-router-dom';
 import Main from "../Main/Main";
 import Login from '../Login/Login';
 import Profile from "../Profile/Profile";
 import AppBar from "../AppBar/AppBar";
-import {HuldBanner} from "../HuldBanner/HuldBanner";
-import {render} from "react-dom";
 
 
+
+/*
+    The root app functionality and browser routing system.
+    Contains always on display AppBar
+    TODO: And handles page authentication checks
+    -AK
+ */
 const App = () => {
     /*
-    TODO: Token handling, better routing system
+    TODO: Token handling
     TODO: Move HuldBanner to app.js
      */
     //const [token, setToken] = useState();
     // if(!token) {
     //     return <Login setToken={setToken} />
     // }
-
         return (
             <div className="wrapper">
                 <BrowserRouter>
