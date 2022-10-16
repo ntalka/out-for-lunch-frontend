b85e91acd995e34e632b8f63146494ee31fdbde7
dmvihe <ville.herajarvi@tuni.fi>
2022-10-16 22:34:41 +0300

    Password and email check

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -11,6 +11,7 @@ import {
 } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import {themeOptions} from "../Theme/ThemeOptions";
+import {useState} from "react";
 const theme = createTheme(themeOptions);
 
 /*
@@ -22,21 +23,64 @@ TODO: quicksand is not correct one, more styling
  */
 
 
+
+
+
+
+
+
+
 const Register = () => {
 
+
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+        passwordAgain: ""
+    });
+
+    //handle change on email
+    const handleChangeEmail = email => event => {
+        setValues({...values, [email]: event.target.value});
+    };
+
+    //handle change on password
+    const handleChangePassword = password => event => {
+        setValues({...values, [password]: event.target.value});
+    };
+
+
+    // handle change on password again
+    const handleChangePasswordAgain = passwordAgain => event => {
+        setValues({...values, [passwordAgain]: event.target.value});
+    };
+
+    // email error
+    const emailError = !values.email.includes("@huld.io");
+
+    // password error
+    const passwordError = !(values.password === values.passwordAgain && values.passwordAgain !== "");
+
+    //register text
     const  registerText = " Check your email after registration to finalize the process!";
+
 
 
     // handle registration sumbit
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
+
         console.log({
             email: data.get('email'),
             password: data.get('password'),
             passwordAgain: data.get("passwordAgain")
         });
     };
+
+
+
+
 
 
     return (
@@ -62,11 +106,16 @@ const Register = () => {
 
                     <TextField label ="Email"
                                id ="email"
+                               value={values.email}
                                name={"email"}
                                type={"email"}
+                               required
                                variant = "outlined"
                                placeholder={"Email"}
                                margin ="normal"
+                               onChange={handleChangeEmail("email")}
+                               helperText={emailError ? "Name needs to be '@huld.io'" : "Perfect!"}
+                               error={emailError}
 
                                // still hard coded borders
                                sx={{
@@ -81,9 +130,14 @@ const Register = () => {
                                name={"password"}
                                type={"password"}
                                label="Password"
+                               required
+                               values={values.password}
                                variant = "outlined"
                                placeholder = "Password"
                                margin ="normal"
+                               onChange={handleChangePassword("password")}
+                               helperText={passwordError ? "Both passwords need to be same" : "Perfect!"}
+                               error={passwordError}
 
                                // still hard coded borders
                                sx={{
@@ -98,12 +152,17 @@ const Register = () => {
 
                     <TextField id={"passwordAgain"}
                                border
+                               required
                                label = "Password again"
                                name={"passwordAgain"}
                                type={"password"}
                                variant = "outlined"
                                placeholder={"Password again"}
                                margin ="normal"
+                               values={values.passwordAgain}
+                               onChange={handleChangePasswordAgain("passwordAgain")}
+                               helperText={passwordError ? "Both passwords need to be same" : "Perfect!"}
+                               error={passwordError}
 
                                // still hard coded borders
                                sx={{
@@ -116,10 +175,13 @@ const Register = () => {
                     />
                     <Typography sx={{fontFamily: 'Quicksand'}} maxWidth={170} textAlign={"center"} > {registerText} </Typography>
                     <Button id={"RegisterButton"}
+                            disabled={emailError || passwordError}
+                            disableElevation
                             sx={{ marginTop: 2, borderRadius: 2 }}
                             style={{fontFamily: 'Quicksand', fontStyle: 'bold', minWidth:'120px'}}
                             type={"submit"}
                             variant = "contained"> Register
+
                     </Button>
                 </Box>
             </form>
@@ -128,5 +190,6 @@ const Register = () => {
     );
 
 };
+
 
 export default Register;
