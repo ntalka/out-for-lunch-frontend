506dd32a3bd060db82f4507e2c2ff4f91d658cc0
dmvihe <ville.herajarvi@tuni.fi>
2022-10-24 00:55:48 +0300

    Profile skeleton

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import {
+    Box,
+    Button,
+    Container, createTheme,
+    CssBaseline, Divider, Grid,
+    ThemeProvider
+} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import {themeOptions} from "../../utils/Theme/ThemeOptions";
+const theme = createTheme(themeOptions);
 
-export default function Profile() {
+const Profile = () => {
     return(
-        <h2>Profile page for user</h2>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box margin="auto"  justifyContent="center" sx={{ width: '100%', maxWidth: 360 }}>
+                    <Typography  fontSize={25} textAlign={"center"} > Your Profile</Typography>
+                    <Typography  textAlign={"center"}> Your profile </Typography>
+                    <Divider justifyContent="center" variant="middle"
+                             sx={{borderBottomWidth: 3}}/>
+                    <Typography  textAlign={"center"}> Testi </Typography>
+                </Box>
+
+
+
+            </Container>
+        </ThemeProvider>
+
     );
-}
+
+};
+
+export default Profile;
