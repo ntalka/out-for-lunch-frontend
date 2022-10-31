65573a407f52b012fa60c62733f36ad6636a4319
dmvihe <ville.herajarvi@tuni.fi>
2022-10-31 02:47:49 +0200

    Removed change password page

--- src/App/App.js
+++ src/App/App.js
@@ -19,7 +19,7 @@ import {
 import NoPermission from "../pages/NoPermission/NoPermission";
 import YourGroup from "../pages/YourGroup/YourGroup";
 import Verify from "../pages/Verify/Verify";
-import ProfileChangePassword from "../pages/Profile/ProfileChangePassword";
+
 
 
 
@@ -50,7 +50,6 @@ const App = () => {
                                     <Route element={<Authenticate/>}>
                                         <Route path="/main" element={<Main />} />
                                         <Route path="/profile" element={<Profile />} />
-                                        <Route path="/profile/changepassword" element={<ProfileChangePassword />} />
                                         <Route path="/yourgroup" element={<YourGroup />} />
                                     </Route>
                                 </Routes>
