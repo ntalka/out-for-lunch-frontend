20a29b2ca6c5176e040c65d89fb0020221ea719b
Aleksi Karelius <aleksi.karelius@tuni.fi>
2022-11-06 20:19:25 +0200

    functionality to the buttoninfo

--- src/components/RestaurantInfoButton/RestaurantInfoButton.js
+++ src/components/RestaurantInfoButton/RestaurantInfoButton.js
@@ -3,26 +3,39 @@ import Typography from "@mui/material/Typography";
 import { Grid, ListItemText} from "@mui/material";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import {useState} from "react";
 
 
-const testRestaurantInfo={
-    name: "TestiRavintola pitkä nimi",
-    time: "11:00",
-    ppl: 55,
-}
 
 /* Restaurant infobutton, to show your selected restaurant for the group,
 time and number of participants
-TODO: functionality, currently static
 - AK
 * */
 export function RestaurantInfoButton() {
-    return(
+    let myGroup;
+    try{
+         myGroup = JSON.parse(sessionStorage.getItem("myGroup"));
+    }
+    catch (e){
+        myGroup = null;
+    }
 
+
+    const [groupId, ] = useState(myGroup ? myGroup["id"] : "null");
+    const [name, ] = useState(myGroup ? myGroup["restaurant"] : null);
+    //const [restaurantId, ] = useState(myGroup ? myGroup["restaurantId"] : null);
+    const [participants, ] = useState(myGroup ? myGroup["participants"] : null);
+    const [time, ] = useState(myGroup ? myGroup["time"] : null);
+    const [hidden, ] = useState(groupId!=="null");
+
+    return(
+        <>
             <ListItemText>
                 <Typography variant={"body1"} width={"100%"} textAlign={"center"} >Your group:</Typography>
-                <Typography variant={"body2"} width={"100%"} textAlign={"center"} >{testRestaurantInfo.name}</Typography>
-
+                {hidden &&
+                    <Typography variant={"body2"} width={"100%"}
+                                textAlign={"center"}>{name}</Typography>
+                }
                 {/*Using grid to easily fit icons and text, can be changed as
                 necessary*/}
                 <Grid marginTop={1} paddingLeft={2} container >
@@ -30,16 +43,23 @@ export function RestaurantInfoButton() {
                         <AccessTimeIcon/>
                     </Grid>
                     <Grid paddingLeft={4}  item xs={11}>
-                        <Typography textAlign={"left"} > {testRestaurantInfo.time}</Typography>
+                        {hidden &&
+                            <Typography textAlign={"left"}> {time}</Typography>
+                        }
                     </Grid>
                     <Grid  item xs={1}>
                         <PeopleAltIcon/>
                     </Grid>
                     <Grid paddingLeft={4} item xs={11}>
-                        <Typography textAlign={"left"} > {testRestaurantInfo.ppl}</Typography>
+                        {hidden &&
+                            <Typography
+                                textAlign={"left"}> {participants}</Typography>
+                        }
                     </Grid>
                 </Grid>
             </ListItemText>
+        </>
+
 
 
 
