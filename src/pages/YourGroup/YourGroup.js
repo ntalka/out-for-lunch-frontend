1a0d4a4e3b828bf0ed929c1c1890a4509a15ce11
Aleksi Karelius <aleksi.karelius@tuni.fi>
2022-11-13 19:56:07 +0200

    Switched using myGroup session data and to using time util and update to use new version of dropdownmenu

--- src/pages/YourGroup/YourGroup.js
+++ src/pages/YourGroup/YourGroup.js
@@ -9,7 +9,7 @@ import {
     SingleGroupDropDown
 } from "../../components/GroupDropDown/GroupDropDown";
 import React from "react";
-import {themeOptions} from "../../utils/Theme/ThemeOptions";
+import {themeOptions} from "../../utils/ThemeOptions";
 import {useState} from "react";
 
 const theme = createTheme(themeOptions)
@@ -22,13 +22,6 @@ const YourGroup = () => {
     catch (e){
         myGroup = null;
     }
-    const [groupId] = useState(myGroup ? myGroup["id"] : "null");
-    const [name] = useState(myGroup ? myGroup["restaurant"] : null);
-    const [restaurantId] = useState(myGroup ? myGroup["restaurantId"] : null);
-    const [participants] = useState(myGroup ? myGroup["participants"] : null);
-    const [time] = useState(myGroup ? myGroup["time"] : null);
-    //const [hidden, setHidden] = useState(groupId!=="null");
-
     return (
         <ThemeProvider theme={theme}>
             <Container component="main" maxWidth="xs">
@@ -43,13 +36,7 @@ const YourGroup = () => {
                 </Box>
                 {myGroup &&
                 <SingleGroupDropDown
-                    key={groupId}
-                    groupId={groupId}
-                    placeId={restaurantId}
-                    placeName={name}
-                    nParticipants={participants}
-                    time={time}
-                    defaultOpen={true}/>
+                    groupData={myGroup}/>
                 }
             </Container>
         </ThemeProvider>
