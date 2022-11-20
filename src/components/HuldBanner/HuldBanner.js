e5ac566786a9c97e548be612794e9c162bd7ba91
Aleksi Karelius <aleksi.karelius@tuni.fi>
2022-11-20 14:12:51 +0200

    Fixed size and alignment issues

--- src/components/HuldBanner/HuldBanner.js
+++ src/components/HuldBanner/HuldBanner.js
@@ -1,4 +1,4 @@
-import { Box, Divider} from "@mui/material";
+import { Box} from "@mui/material";
 import logo_white from '../../material/huld_logo_white.svg';
 import {CenterDivider} from "../StyledMui/CenterDivider";
 
@@ -12,9 +12,9 @@ Aleksi Karelius
 export function HuldBanner() {
     return (
         /* Centered banner, currently margined const 3 from top*/
-        <Box alignItems={"center"} margin="auto" marginTop= {6} justifyContent="center" >
+        <Box maxWidth={360}  marginTop= {6} justifyContent="center" >
             <CenterDivider/>
-            <Box alignContent='center'>
+            <Box >
                 <a href="https://huld.io">
                     <img alt="Huld company logo" src={logo_white} align='center' />
                 </a>
