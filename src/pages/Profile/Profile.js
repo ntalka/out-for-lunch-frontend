b0621021efbbb9865e2cc2efc7ae398cace6603a
Aleksi Karelius <aleksi.karelius@tuni.fi>
2022-11-27 19:20:29 +0200

    Office selection functionality / fetching offices

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
+    Autocomplete,
     Box,
     Button,
     Container, createTheme,
-    CssBaseline, Divider, FormControl, MenuItem, Select,
+    CssBaseline, Divider, FormControl,
     ThemeProvider
 } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import {themeOptions} from "../../utils/ThemeOptions";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {useAuth} from "../../utils/Authenticate";
+import {getRequest} from "../../utils/RequestUtils";
+import TextField from "@mui/material/TextField";
 
 const theme = createTheme(themeOptions);
 
+
+
 const Profile = () => {
-    const [location, setLocation] = React.useState(10);
+    const {user} = useAuth();
+    const navigate = useNavigate();
+    const [location, setLocation] = React.useState();
+    const [offices, setOffices] = React.useState();
+    const [currentOffice] = React.useState(JSON.parse(sessionStorage.getItem("userInfo"))["officeId"])
 
-    const handleChange = (event) => {
-        setLocation(event.target.value);
-    };
+    // handle autocomplete change to get value
+    const handleAutoChange = (event, value) => setLocation(value);
 
+    // fetch office locations
+    useEffect(()=>{
+        if(!offices){
+            getRequest("/get-offices-list", String(user))
+                .then((resJSON)=>{
+                    if(resJSON){
+                        let data =[];
+                        resJSON["data"].map((value) =>{
+                            let option={
+                                label: value["name"],
+                                id: value["id"]
+                            }
+                            if(option.id === currentOffice){
+                                setLocation(option)
+                            }
+                            data.push(option);
+                            return true
+                        })
+                        setOffices(data);
+                    }
+                })
+        }
+    })
+
+    // save current settings
+    // TODO: There's no endpoint for changing the office
+    function saveSettings() {
+        let currentInfo = JSON.parse(sessionStorage.getItem("userInfo"));
+        currentInfo["officeId"] = {location}["location"]["id"];
+        sessionStorage.setItem("userInfo", JSON.stringify(currentInfo));
+        navigate("/main")
+    }
 
     return(
         <ThemeProvider theme={theme}>
@@ -32,29 +73,32 @@ const Profile = () => {
                             backgroundColor: theme.palette.secondary.dark,
                             '&:hover':{backgroundColor: theme.palette.primary.dark,
                                 color: theme.palette.primary.contrastText}}}>
-
-                        <Select sx={{color: "black", fontWeight: "bold"}}
-                                defaultValue={10}
-                                labelId="select-label"
-                                id="select"
-                                value={location}
-                                label="Location"
-                                onChange={handleChange}
-                        >
-
-                            <MenuItem value={10}>Ratina</MenuItem>
-                            <MenuItem value={20}>Location 2</MenuItem>
-                            <MenuItem value={30}>Location 3</MenuItem>
-                        </Select>
+                        {offices &&
+                            <Autocomplete
+                            disablePortal
+                            variant={"outlined"}
+                            id="officeSelector"
+                            value={location}
+                            options={offices}
+                            isOptionEqualToValue={(option, value) => option.value === value.value}
+                            /* Currently, value on console */
+                            onChange={handleAutoChange}
+                            sx={{
+                            width: 320,
+                            svg: {
+                            color: '#ffffff' },
+                            input: {
+                            color: '#ffffff' },
+                        }}
+                            renderInput={(params) => <TextField {...params}  />}
+                            />
+                        }
                     </FormControl>
                     <Divider  justifycontent="center" variant="middle" sx={{borderBottomWidth: 3}}/>
 
-                    <Button component={Link} to="/main" sx={{ marginTop: 5, marginBottom: -25}}
+                    <Button onClick={saveSettings} to="/main" sx={{ marginTop: 5, marginBottom: -25}}
                             >Back to main page </Button>
                 </Box>
-
-
-
             </Container>
         </ThemeProvider>
 
