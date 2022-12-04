4de7781084a9cdb208e6eac3b58f47636997a3e5
Aleksi Karelius <aleksi.karelius@tuni.fi>
2022-12-05 00:19:40 +0200

    created group utils for joining, creating and deleting groups

--- src/utils/Groups.js
+++ src/utils/Groups.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import {getUser} from "./Authenticate";
-import {getRequest} from "./RequestUtils";
+import {deleteRequest, getRequest, postRequest} from "./RequestUtils";
 
 
 export async function GetAllGroups() {
-    const user = getUser();
-    const resJSON = getRequest("/get-groups-list", String(user))
+    const resJSON = getRequest("/get-groups-list", String(getUser()))
         .then((resJSON) => {
             if(resJSON.status===200){
                 const data = JSON.stringify(resJSON["data"])
@@ -32,4 +31,55 @@ export function GetGroup(groupId){
     }
     return null;
 }
+
+export async function joinGroup(groupId){
+    await postRequest("/join-group/"+groupId, {}, String(getUser()))
+        .then(() =>{
+        });
+
+}
+
+export async function leaveGroup(groupId){
+    await postRequest("/leave-group/"+groupId,{}, String(getUser()))
+        .then(() =>{
+        });
+}
+
+export function deleteGroup(groupId, joined){
+    deleteRequest("/delete-group/"+groupId, String(getUser()))
+        .then(()=>{
+            if(joined){
+                sessionStorage.removeItem("myGroup")
+            }
+            window.location.reload();
+        });
+}
+
+export function joinRandomGroup(startTime, endTime){
+    const body = {
+        "endTime": endTime,
+        "startTime": startTime}
+    postRequest("/join-random-group", body, String(getUser()))
+        .then(() =>{
+        });
+}
+
+export async function createEatAtOffice(startTime){
+    const body = {
+        "time": startTime
+    }
+    await postRequest("/eat-at-office", body, String(getUser()))
+        .then(() =>{
+    });
+}
+
+export async function createGroup(placeId, startTime){
+    const body = {
+        "time": startTime,
+        "restaurantId": placeId}
+    await postRequest("/create-custom-group", body, String(getUser()))
+        .then(() =>{
+        });
+}
+
 
